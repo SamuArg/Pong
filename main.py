7c988eae9dd49cff2c4b5efa6d88434db814f81f
SamA1430 <samuel.arg14@gmail.com>
2024-03-04 13:09:24 -0500

    Create ball movement and collisions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.font.init()
 
 WIDTH = 1000
@@ -16,26 +16,32 @@ BALL_VEL = 5
 PLAYERS_COLOR = "white"
 BALL_COLOR = "white"
 
+
 def draw(player1,player2, ball_position):
     WIN.fill("black")
     pygame.draw.rect(WIN, PLAYERS_COLOR, player1)
     pygame.draw.rect(WIN,PLAYERS_COLOR, player2)
-    pygame.draw.circle(WIN, BALL_COLOR, ball_position, radius=BALL_WIDTH)
+    ball = pygame.draw.circle(WIN, BALL_COLOR,ball_position, radius=BALL_WIDTH)
     pygame.display.update()
+    return ball
 
 def main():
     run = True
+    ball = Ball((BALL_VEL,0))
     clock = pygame.time.Clock()
     player1 = pygame.Rect(0,HEIGHT//2-PLAYERS_HEIGHT//2, PLAYERS_WIDTH, PLAYERS_HEIGHT)
     player2 = pygame.Rect(WIDTH-PLAYERS_WIDTH, HEIGHT//2-PLAYERS_HEIGHT//2, PLAYERS_WIDTH, PLAYERS_HEIGHT)
     ball_position = (WIDTH//2,HEIGHT//2)
-    draw(player1, player2, ball_position)
+    ball_rect = draw(player1, player2, ball_position)
     while run:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        handle_border_collision(ball_rect,ball)    
+        ball_position = (ball_position[0]+ball.direction[0], ball_position[1]+ball.direction[1])
+
         keys = pygame.key.get_pressed()
 
         
@@ -48,8 +54,25 @@ def main():
         if keys[pygame.K_s] and player1.y + PLAYERS_VEL + PLAYERS_HEIGHT <= HEIGHT:
             player1.y += PLAYERS_VEL
         
+        if player1.colliderect(ball_rect):
+            ball.direction = (1*BALL_VEL,random.random()*BALL_VEL)
+        if player2.colliderect(ball_rect):
+            ball.direction = (-1*BALL_VEL, random.random()*BALL_VEL)
 
-        draw(player1, player2, ball_position)
+        ball_rect = draw(player1, player2, ball_position)
+
+
+def handle_border_collision(ball_rect, ball):
+    if ball_rect.y - BALL_VEL <= 0:
+        ball_rect.y += 2*BALL_WIDTH
+        ball.direction = (ball.direction[0], -ball.direction[1])
+    elif ball_rect.y + 2*BALL_WIDTH + BALL_VEL >= HEIGHT:
+        ball_rect.y -= 2*BALL_WIDTH
+        ball.direction = (ball.direction[0], -ball.direction[1])
+
+class Ball:
+    def __init__(self, direction):
+        self.direction = direction
 
 if __name__ == "__main__":
     main()
